4f05fdf375b0d9926607974f6401ac8566942d8f
JelleKomm1500 <damwaldster24@gmail.com>
2018-10-09 22:43:33 +0200

    New skybox

--- js/Skybox.js
+++ js/Skybox.js
@@ -2,12 +2,12 @@
 function Skybox(){
     var skyboxGeometry = new THREE.BoxGeometry( 1000, 1000, 1000);
     var skyboxMaterials = [
-        new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("textures/Skybox/Left.png"), side: THREE.DoubleSide}),
-        new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("textures/Skybox/Right.png"), side: THREE.DoubleSide}),
-        new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("textures/Skybox/Up.png"), side: THREE.DoubleSide}),
-        new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("textures/Skybox/Down.png"), side: THREE.DoubleSide}),
-        new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("textures/Skybox/Front.png"), side: THREE.DoubleSide}),
-        new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("textures/Skybox/Back.png"), side: THREE.DoubleSide}),
+        new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("textures/Skybox/Front1.png"), side: THREE.DoubleSide}),
+        new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("textures/Skybox/Back1.png"), side: THREE.DoubleSide}),
+        new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("textures/Skybox/Up1.png"), side: THREE.DoubleSide}),
+        new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("textures/Skybox/Down1.png"), side: THREE.DoubleSide}),
+        new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("textures/Skybox/Right1.png"), side: THREE.DoubleSide}),
+        new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("textures/Skybox/Left1.png"), side: THREE.DoubleSide}),
     ];
     var skyboxMaterial = new THREE.MeshFaceMaterial(skyboxMaterials);
     var skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
