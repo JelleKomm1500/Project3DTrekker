83baca9db81eb18b75a3eff5736db3fcbbc58a46
JelleKomm1500 <32571630+JelleKomm1500@users.noreply.github.com>
2018-10-09 12:56:25 +0200

    Objloader works
    ps Redmer is gay

---
+++ js/Bulldozer.js
@@ -0,0 +1,8 @@
+﻿function Bulldozer() {
+    var bullGroup = new THREE.Group();
+
+    LoadOBJModel("objects/trekker/", "Tractor.obj", "objects/trekker/", "Tractor.mtl", (mesh) => {
+        //mesh.children[0].material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+        return mesh;
+    })
+}
