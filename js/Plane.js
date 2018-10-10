7c0b021801e919b8c396548632a50d00f68d10bb
JelleKomm1500 <32571630+JelleKomm1500@users.noreply.github.com>
2018-10-10 13:21:18 +0200

    Shit deleted + skybox and plane added

---
+++ js/Plane.js
@@ -0,0 +1,14 @@
+﻿function Plane() {
+    var geometry = new THREE.PlaneGeometry(2000, 2000, 2000);
+
+    var stoeptexture = new THREE.TextureLoader().load("textures/plane/grass1.jpg");
+    stoeptexture.wrapS = THREE.RepeatWrapping;
+    stoeptexture.wrapT = THREE.RepeatWrapping;
+    stoeptexture.repeat.set(40, 40);
+
+    var material = new THREE.MeshBasicMaterial({ map: stoeptexture, side: THREE.DoubleSide });
+
+    var plane = new THREE.Mesh(geometry, material);
+    plane.rotateX(- Math.PI / 2);
+    return plane;
+}
--- js/Skybox.js
+++ js/Skybox.js
@@ -1,16 +1,8 @@
-//Adds skybox
-function Skybox(){
-    var skyboxGeometry = new THREE.BoxGeometry( 1000, 1000, 1000);
-    var skyboxMaterials = [
-        new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("textures/Skybox/Front1.png"), side: THREE.DoubleSide}),
-        new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("textures/Skybox/Back1.png"), side: THREE.DoubleSide}),
-        new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("textures/Skybox/Up1.png"), side: THREE.DoubleSide}),
-        new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("textures/Skybox/Down1.png"), side: THREE.DoubleSide}),
-        new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("textures/Skybox/Right1.png"), side: THREE.DoubleSide}),
-        new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("textures/Skybox/Left1.png"), side: THREE.DoubleSide}),
-    ];
-    var skyboxMaterial = new THREE.MeshFaceMaterial(skyboxMaterials);
-    var skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
-
-    return skybox;
+function CircleSkyBox() {
+    var sphericalSkyboxGemotery = new THREE.SphereGeometry(675, 64, 64);
+    var sphericalSkyboxMaterial = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("textures/CircleSkybox/panorama.png"), side: THREE.DoubleSide });
+    var sphericalSkybox = new THREE.Mesh(sphericalSkyboxGemotery, sphericalSkyboxMaterial);
+    sphericalSkybox.rotateX(Math.PI / 75)
+    sphericalSkybox.rotateZ(Math.PI / 275)
+    return sphericalSkybox;
 }
