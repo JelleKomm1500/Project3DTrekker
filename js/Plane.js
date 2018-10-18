3a77df14ea6a53e9189ac7d07e2c135f142ae8be
sjimmie bleeker <blee1703@student.nhl.nl>
2018-10-18 10:14:56 +0200

    test

--- js/Plane.js
+++ js/Plane.js
@@ -1,10 +1,10 @@
 ﻿function Plane() {
-    var geometry = new THREE.PlaneGeometry(2000, 2000, 2000);
+    var geometry = new THREE.PlaneGeometry(19999, 19999, 19999);
 
     var stoeptexture = new THREE.TextureLoader().load("textures/plane/grass1.jpg");
     stoeptexture.wrapS = THREE.RepeatWrapping;
     stoeptexture.wrapT = THREE.RepeatWrapping;
-    stoeptexture.repeat.set(40, 40);
+    stoeptexture.repeat.set(750, 750);
 
     var material = new THREE.MeshBasicMaterial({ map: stoeptexture, side: THREE.DoubleSide });
 
