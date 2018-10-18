c15e0821b5c22cb96cd36ae10fdfa5517637a717
sjimmie bleeker <blee1703@student.nhl.nl>
2018-10-18 12:26:37 +0200

    push

--- js/Plane.js
+++ js/Plane.js
@@ -1,5 +1,5 @@
 ﻿function Plane() {
-    var geometry = new THREE.PlaneGeometry(19999, 19999, 19999);
+    var geometry = new THREE.PlaneGeometry(19999, 19999);
 
     var stoeptexture = new THREE.TextureLoader().load("textures/plane/grass1.jpg");
     stoeptexture.wrapS = THREE.RepeatWrapping;
