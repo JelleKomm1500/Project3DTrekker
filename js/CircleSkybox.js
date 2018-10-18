b42da2179990f2f9742bc61cac31f2c4841b079d
sjimmie bleeker <blee1703@student.nhl.nl>
2018-10-18 10:19:44 +0200

    circleskybox back at it again

---
+++ js/CircleSkybox.js
@@ -0,0 +1,9 @@
+﻿
+function CircleSkyBox() {
+    var sphericalSkyboxGemotery = new THREE.SphereGeometry(675, 64, 64);
+    var sphericalSkyboxMaterial = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("textures/CircleSkybox/panorama.png"), side: THREE.DoubleSide });
+    var sphericalSkybox = new THREE.Mesh(sphericalSkyboxGemotery, sphericalSkyboxMaterial);
+    sphericalSkybox.rotateX(Math.PI / 75)
+    sphericalSkybox.rotateZ(Math.PI / 275)
+    return sphericalSkybox;
+}
