79a72b422a68a0cb8414ab5184203bf541ea939b
sjimmie bleeker <blee1703@student.nhl.nl>
2018-10-18 10:46:18 +0200

    CircleSkybox back at it again

--- js/CircleSkybox.js
+++ js/CircleSkybox.js
@@ -1,7 +1,7 @@
 ﻿
 function CircleSkyBox() {
-    var sphericalSkyboxGemotery = new THREE.SphereGeometry(675, 64, 64);
-    var sphericalSkyboxMaterial = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("textures/CircleSkybox/panorama.png"), side: THREE.DoubleSide });
+    var sphericalSkyboxGemotery = new THREE.SphereGeometry(19999, 256, 256);
+    var sphericalSkyboxMaterial = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("textures/CircleSkybox/panorama3.png"), side: THREE.DoubleSide });
     var sphericalSkybox = new THREE.Mesh(sphericalSkyboxGemotery, sphericalSkyboxMaterial);
     sphericalSkybox.rotateX(Math.PI / 75)
     sphericalSkybox.rotateZ(Math.PI / 275)
---
+++ js/DikkeRoad.js
@@ -0,0 +1,14 @@
+﻿function DikkeRoad() {
+    var geometry = new THREE.PlaneGeometry(19999, 19999, 19999);
+
+    var stoeptexture = new THREE.TextureLoader().load("textures/plane/grass1.jpg");
+    stoeptexture.wrapS = THREE.RepeatWrapping;
+    stoeptexture.wrapT = THREE.RepeatWrapping;
+    stoeptexture.repeat.set(750, 750);
+
+    var material = new THREE.MeshBasicMaterial({ map: stoeptexture, side: THREE.DoubleSide });
+
+    var plane = new THREE.Mesh(geometry, material);
+    plane.rotateX(- Math.PI / 2);
+    return plane;
+}
