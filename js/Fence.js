18f594a181058c324693d633c8944a1f13c7264c
Jan <tuin1705@student.nhl.nl>
2018-10-19 12:13:47 +0200

    SwitCommit

---
+++ js/Fence.js
@@ -0,0 +1,10 @@
+﻿function Fence() {
+    var Fence = new THREE.Group();
+
+    LoadOBJModel("objects/chalet/", "CUPIC_CHALET.obj", "objects/chalet/", "CUPIC_CHALET.mtl", (mesh) => {
+        mesh.rotation.set(0, 3.14, 0);
+        mesh.scale.set(0.5, 0.5, 0.5);
+        Fence.add(mesh);
+    })
+    return Fence;
+}
---
+++ js/TestObject.js
@@ -0,0 +1,10 @@
+﻿function ColissionObject() {
+    var TestObject = new THREE.Object3D();
+
+    LoadOBJModel("objects/fence/", "cube.obj", "objects/fence/", "cube.mtl", (mesh) => {
+        mesh.rotation.set(0, 3.14, 0)
+
+        TestObject.add(mesh);
+    })
+    return TestObject;
+}
