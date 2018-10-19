7d69c0c1d10a378d250483a5926a570072f44aa1
Redmar Wijbenga <wijb1704@student.nhl.nl>
2018-10-19 11:17:32 +0200

    summiere summary

---
+++ js/tires.js
@@ -0,0 +1,10 @@
+﻿function tires() {
+    var Tires = new THREE.Group();
+
+    LoadOBJModel("objects/tires/", "tires.obj", "objects/tires/", "tires.mtl", (mesh) => {
+        mesh.scale.set(10, 10, 10);
+        mesh.rotation.set(0, 3.14, 0)
+        Tires.add(mesh);
+    })
+    return Tires;
+}
