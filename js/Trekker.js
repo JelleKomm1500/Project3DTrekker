2c7b3132720aa7f8801d315ee77af673ae045fab
Jan Tuin <tuin1705@student.nhl.nl>
2018-10-30 10:53:26 +0100

    asdsad

--- js/Trekker.js
+++ js/Trekker.js
@@ -1,13 +1,12 @@
-﻿function Trekker(keuze) {
+﻿function Trekker() {
     console.log("asdsadtrekkerrr");
     var boxgroep = new THREE.Group();
 
         var CollisionBox = new Physijs.BoxMesh(
             new THREE.BoxGeometry(5, 5, 5),
-            new THREE.MeshBasicMaterial({ color: 0xa7f442, transparent: true, opacity:0 }), 1
-        );
-
-
+            new THREE.MeshBasicMaterial({ color: 0xa7f442, transparent: true, opacity:1 }), 1
+    );
+   
         LoadOBJModel("objects/trekker/", "Tractor.obj", "objects/trekker/", "Tractor.mtl", (mesh) => {
             mesh.rotation.set(0, 3.14, 0)
 
@@ -17,7 +16,7 @@
 
         boxgroep.add(CollisionBox);
 
-        return CollisionBox;
+    return CollisionBox;
         
     
     
