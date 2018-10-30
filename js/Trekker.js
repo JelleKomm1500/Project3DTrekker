e52bad10a5bb2370454ac8895ee2e37708692558
Jan Tuin <tuin1705@student.nhl.nl>
2018-10-30 11:18:41 +0100

    Trekker zit nu goed in de box

--- js/Trekker.js
+++ js/Trekker.js
@@ -3,19 +3,19 @@
     var boxgroep = new THREE.Group();
 
         var CollisionBox = new Physijs.BoxMesh(
-            new THREE.BoxGeometry(5, 5, 5),
+            new THREE.BoxGeometry(10, 10, 10),
             new THREE.MeshBasicMaterial({ color: 0xa7f442, transparent: true, opacity:1 }), 1
     );
    
         LoadOBJModel("objects/trekker/", "Tractor.obj", "objects/trekker/", "Tractor.mtl", (mesh) => {
             mesh.rotation.set(0, 3.14, 0)
-
+            mesh.position.y = -5;
             CollisionBox.add(mesh);
 
         })
 
-        boxgroep.add(CollisionBox);
-
+    boxgroep.add(CollisionBox);
+   
     return CollisionBox;
         
     
