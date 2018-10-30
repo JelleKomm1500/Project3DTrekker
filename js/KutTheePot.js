f09294d03d505475d06b1d42711fdba79b434b68
Jan Tuin <tuin1705@student.nhl.nl>
2018-10-30 12:17:13 +0100

    SAASDSAD

---
+++ js/KutTheePot.js
@@ -0,0 +1,12 @@
+﻿function TheePot() {
+    var theepot;
+    // BEGIN Clara.io JSON loader code
+    var objectLoader = new THREE.ObjectLoader();
+    objectLoader.load("objects/heuvelmap.json", function (obj) {
+        return obj;
+        
+    });
+            // END Clara.io JSON loader code
+
+
+}
