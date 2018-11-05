89eafb9caccdf0452ec16aaf7c3a250f25184437
JelleKomm1500 <32571630+JelleKomm1500@users.noreply.github.com>
2018-11-05 19:46:28 +0100

    Fix?

--- js/RemovePlayer.js
+++
@@ -1,9 +0,0 @@
-﻿function RemovePlayer() {
-    var select = document.getElementById("addPlayerCard");
-    select.removeChild(select.lastChild);
-
-    if (select.childElementCount === 1) {
-        var x = document.getElementById("minus");
-        x.style.display = "none";
-    }
-}
