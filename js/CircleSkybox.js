b294b05484b5827e634cc6962784c2b12b9f89e0
JelleKomm1500 <32571630+JelleKomm1500@users.noreply.github.com>
2018-11-06 16:19:13 +0100

    Javascript files verwijdert en links weggehaald

--- js/CircleSkybox.js
+++
@@ -1,9 +0,0 @@
-﻿
-function CircleSkyBox() {
-    var sphericalSkyboxGemotery = new THREE.SphereGeometry(19999, 256, 256);
-    var sphericalSkyboxMaterial = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("textures/CircleSkybox/panorama3.png"), side: THREE.DoubleSide });
-    var sphericalSkybox = new THREE.Mesh(sphericalSkyboxGemotery, sphericalSkyboxMaterial);
-    sphericalSkybox.rotateX(Math.PI / 75)
-    sphericalSkybox.rotateZ(Math.PI / 275)
-    return sphericalSkybox;
-}
--- js/Light.js
+++
@@ -1,4 +0,0 @@
-﻿function Light() {
-    
-
-}
