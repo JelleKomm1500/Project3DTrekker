4e0b4b7883f4c958a57bb4458941a9e8fec47135
Jan Tuin <tuin1705@student.nhl.nl>
2018-11-06 10:07:54 +0100

    BullDozer Rotation

--- js/ZwareTrekker.js
+++ js/ZwareTrekker.js
@@ -3,21 +3,21 @@
     //nog cylinderwielen proberen?
     var CollisionBox = new Physijs.BoxMesh(
         new THREE.BoxGeometry(45, 10, 10),
-        new THREE.MeshBasicMaterial({ color: 0xa7f442, transparent: false, opacity: 0 }), 1
+        new THREE.MeshBasicMaterial({ color: 0xa7f442, transparent: true, opacity: 0 }), 1
     );
 
 
     var CollisionBox2 = new Physijs.CylinderMesh(
         new THREE.CylinderGeometry(4, 4, 2, 24),
-        new THREE.MeshBasicMaterial({ color: 0xa3272, transparent: false, opacity: 1 }), 1
+        new THREE.MeshBasicMaterial({ color: 0xa3272, transparent: true, opacity: 0 }), 1
     );
 
     var voorkant = new Physijs.BoxMesh(
         new THREE.BoxGeometry(10, 10, 30),
         new THREE.MeshBasicMaterial({ color: 0xa7f442, transparent: false, opacity: 0 }), 1
     );
-    voorkant.position.set(-15, 0, 0);
-
+    voorkant.position.set(0, 0, -15);
+    voorkant.rotation.set(0, 1.57, 0);
    CollisionBox.add(voorkant);
 
 
@@ -26,8 +26,8 @@
         new THREE.MeshBasicMaterial({ color: 0xa7f442, transparent: false, opacity: 0 }), 1
     );
 
-    achterkant.position.set(40, 0, 0);
-
+    achterkant.position.set(0, 0, 35);
+    achterkant.rotation.set(0, 1.57, 0);
     CollisionBox.add(achterkant);
 
     var BinnenKant = new Physijs.CylinderMesh(
@@ -35,11 +35,12 @@
         new THREE.MeshBasicMaterial({ color: 0xa3272, transparent: false, opacity: 1 }), 1
     );
 
-    BinnenKant.position.set(15, 0, 0);
+    BinnenKant.position.set(0, 0, 10);
 
     CollisionBox.add(BinnenKant);
 
     LoadOBJModel("objects/bulldozer/", "Bulldozer.obj", "objects/bulldozer/", "Bulldozer.mtl", (mesh) => {
+        mesh.rotation.set(0, 1.57, 0);
         mesh.position.set(0, 0, 0);
         mesh.scale.set(0.2, 0.2, 0.2);
         CollisionBox.add(mesh);
@@ -49,7 +50,6 @@
 
     CollisionBox.add(text);
     CollisionBox.scale.set(1, 1, 1);
-    CollisionBox.rotation.set(0, 1.57, 0);
 
     return CollisionBox;
 }
