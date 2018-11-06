abc01541810ea1244bf5fa3b15ea4f8b49ea2587
sjimmie bleeker <blee1703@student.nhl.nl>
2018-11-06 16:03:56 +0100

    inputvalidation fixed

---
+++ js/InputValidation.js
@@ -0,0 +1,20 @@
+﻿function InputValidation(settingsarray) {
+    var inputbool = true;
+    for (var i = 0; i < (settingsarray.length); i++) {
+        var eendArray = settingsarray[i];
+        if (!(eendArray[4] == "")) {
+            for (var i2 = 2; i2 < 5; i2++) {
+                if (eendArray[i2].length > 1) { inputbool = false; }
+            }
+        }
+
+
+    }
+
+    if (inputbool) {
+        StartGame(settingsarray);
+    }
+    else {
+        alert("De controls mogen maar 1 karakter lang zijn!");
+    }
+}
