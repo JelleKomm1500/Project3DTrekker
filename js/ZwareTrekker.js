157a8ac0dd7405faa76899c1fc1ab892ee3371de
sjimmie bleeker <blee1703@student.nhl.nl>
2018-11-06 10:20:16 +0100

    bulldozer text en position

--- js/ZwareTrekker.js
+++ js/ZwareTrekker.js
@@ -14,7 +14,7 @@
 
     var voorkant = new Physijs.BoxMesh(
         new THREE.BoxGeometry(10, 10, 30),
-        new THREE.MeshBasicMaterial({ color: 0xa7f442, transparent: false, opacity: 0 }), 1
+        new THREE.MeshBasicMaterial({ color: 0xa7f442, transparent: true, opacity: 0 }), 1
     );
     voorkant.position.set(0, 0, -15);
     voorkant.rotation.set(0, 1.57, 0);
@@ -23,7 +23,7 @@
 
     var achterkant = new Physijs.BoxMesh(
         new THREE.BoxGeometry(10, 10, 30),
-        new THREE.MeshBasicMaterial({ color: 0xa7f442, transparent: false, opacity: 0 }), 1
+        new THREE.MeshBasicMaterial({ color: 0xa7f442, transparent: true, opacity: 0 }), 1
     );
 
     achterkant.position.set(0, 0, 35);
@@ -32,7 +32,7 @@
 
     var BinnenKant = new Physijs.CylinderMesh(
         new THREE.CylinderGeometry(20, 20, 20, 50),
-        new THREE.MeshBasicMaterial({ color: 0xa3272, transparent: false, opacity: 1 }), 1
+        new THREE.MeshBasicMaterial({ color: 0xa3272, transparent: true, opacity: 0 }), 1
     );
 
     BinnenKant.position.set(0, 0, 10);
@@ -41,12 +41,14 @@
 
     LoadOBJModel("objects/bulldozer/", "Bulldozer.obj", "objects/bulldozer/", "Bulldozer.mtl", (mesh) => {
         mesh.rotation.set(0, 1.57, 0);
-        mesh.position.set(0, 0, 0);
+        mesh.position.set(0, 5, 0);
         mesh.scale.set(0.2, 0.2, 0.2);
         CollisionBox.add(mesh);
     })
 
     var text = CreatePlayerText(spelernaam);
+    text.scale.set(2, 2, 2);
+    text.position.set(0, 5, 0);
 
     CollisionBox.add(text);
     CollisionBox.scale.set(1, 1, 1);
