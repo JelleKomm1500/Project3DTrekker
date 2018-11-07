7df30fc0d84be837a0ed067e0bb7d1672e406b40
JelleKomm1500 <32571630+JelleKomm1500@users.noreply.github.com>
2018-11-07 09:55:50 +0100

    Music

---
+++ js/PlayMusic.js
@@ -0,0 +1,41 @@
+﻿var audio = new Audio("music/Johnny_Cash_-_General_Lee.mp3");
+
+function PlayMusic() {
+    var x = document.getElementById("play");
+    var y = document.getElementById("mute");
+
+    if (x.style.display === "block") {
+        x.style.display = "none";
+        y.style.display = "block";
+    } else {
+        x.style.display = "none";
+        y.style.display = "block";
+    }
+
+    audio.play();
+
+}
+
+function StopMusic() {
+    var x = document.getElementById("play");
+    var y = document.getElementById("mute");
+    if (y.style.display === "block") {
+        y.style.display = "none";
+        x.style.display = "block";
+    } else {
+        y.style.display = "block";
+        x.style.display = "none";
+    }
+
+    audio.pause();
+}
+
+function PauseMusic() {
+    var x = document.getElementById("play");
+    var y = document.getElementById("mute");
+    if (y.style.display === "block" || x.style.display === "block") {
+        x.style.display = "none";
+        y.style.display = "none";
+    }
+    audio.pause();
+}
